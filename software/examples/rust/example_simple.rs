f5c090f5181e9d2f55889d57ac582d50f467b881
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:22:39 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,23 +1,23 @@
 use std::{error::Error, io, thread, time::Duration};
-use tinkerforge::{industrial_dual_relay_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{industrial_dual_relay_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Dual Relay Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Dual Relay Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let industrial_dual_relay_bricklet = IndustrialDualRelayBricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let idr = IndustrialDualRelayBricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
     // Turn relays alternating on/off 10 times with 1 second delay
     for i in 0..5 {
         thread::sleep(Duration::from_millis(1000));
-        industrial_dual_relay_bricklet.set_value(true, false);
+        idr.set_value(true, false);
         thread::sleep(Duration::from_millis(1000));
-        industrial_dual_relay_bricklet.set_value(false, true);
+        idr.set_value(false, true);
     }
 
     println!("Press enter to exit.");
