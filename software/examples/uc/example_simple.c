731bc4cf44057f505838892a68881b28f4d0ba72
Matthias Bolte <matthias@tinkerforge.com>
2022-04-27 15:46:07 +0200

    uc: Adapt to bindings changes

--- software/examples/uc/example_simple.c
+++ software/examples/uc/example_simple.c
@@ -1,23 +1,19 @@
 // This example is not self-contained.
-// It requres usage of the example driver specific to your platform.
+// It requires usage of the example driver specific to your platform.
 // See the HAL documentation.
 
-#include "bindings/hal_common.h"
-#include "bindings/bricklet_industrial_dual_relay.h"
+#include "src/bindings/hal_common.h"
+#include "src/bindings/bricklet_industrial_dual_relay.h"
 
-#define UID "XYZ" // Change XYZ to the UID of your Industrial Dual Relay Bricklet
-
-void check(int rc, const char* msg);
-
+void check(int rc, const char *msg);
 void example_setup(TF_HAL *hal);
 void example_loop(TF_HAL *hal);
-
 
 static TF_IndustrialDualRelay idr;
 
 void example_setup(TF_HAL *hal) {
 	// Create device object
-	check(tf_industrial_dual_relay_create(&idr, UID, hal), "create device object");
+	check(tf_industrial_dual_relay_create(&idr, NULL, hal), "create device object");
 
 	// Turn relays alternating on/off 10 times with 1 second delay
 	int i;
